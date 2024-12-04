ec2247ebe0400d24add07cbf130baf354f6fe89e
RayMiller7 <raymond@doohit.fr>
2024-12-04 16:44:39 +0100

    ajout des composants

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ProductTable from "./components/ProductTable";
+import ProductForm from "./components/ProductForm";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Gestion des Produits</h1>
+      <ProductForm />
+      <ProductTable />
     </div>
   );
-}
+};
 
 export default App;
